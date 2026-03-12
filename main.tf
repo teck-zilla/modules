0a2c869f28346d7fd160a88d31a8cb7d1185159e
teck-zilla <105717995+teck-zilla@users.noreply.github.com>
2026-03-12 11:12:57 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "aws_vpc" "My_VPC" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.name
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+variable "name" {
+  description = "Name of the VPC"
+  type        = string
+}
